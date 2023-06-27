b2ed386de51addefb70c5fbc85b68f42e3dcf058
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-06-27 10:46:45 +0100

    added login page from net now time to implement other stuff

--- quizapp/src/main/java/com/quiz/quizapp/repository/UserRepository.java
+++ quizapp/src/main/java/com/quiz/quizapp/repository/UserRepository.java
@@ -1,9 +1,10 @@
-package com.quiz.quizapp.user;
+package com.quiz.quizapp.repository;
 
+import com.quiz.quizapp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    public User findByEmail(String email);
+    User findByEmail(String email);
 }
